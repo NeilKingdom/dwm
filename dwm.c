e8f7c1ca343a4749c2572a6827d6b78049da0b8b
NeilKingdom <neil@neilkingdom.xyz>
2022-12-26 23:21:27 -0500

    Added centered window name patch

--- dwm.c
+++ dwm.c
@@ -743,8 +743,12 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
+         /* Fix overflow when window name is bigger than window width */
+         int mid = (m->ww - (int)TEXTW(m->sel->name)) / 2 - x;
+         /* Make sure name will not overlap on tags even when it is very long */
+         mid = mid >= lrpad / 2 ? mid : lrpad / 2;
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w - (2 * sp), bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w - (2 * sp), bh, mid, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
