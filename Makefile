0fc2925f002d9f9b4239f25a8d64cea2604f24d9
NeilKingdom <neil@neilkingdom.xyz>
2022-12-27 19:53:10 -0500

    Added mpd control patch

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ options:
 .c.o:
 	${CC} -c ${CFLAGS} $<
 
-${OBJ}: config.h config.mk
+${OBJ}: config.h config.mk mpdcontrol.c
 
 config.h:
 	cp config.def.h $@
--- config.def.h
+++ config.def.h
@@ -1,3 +1,5 @@
+#include "mpdcontrol.c"
+
 /*
    The default configuration file for DWM.
    Patches from suckless.org will be applied to this file.
@@ -26,15 +28,15 @@ static const char *fonts[] = {
 };
 
 /* Color pallet */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#BBBBBB";
-static const char col_gray4[]       = "#EEEEEE";
-static const char col_cyan[]        = "#005577";
-static const char col_purple[]      = "#9544FF";
+static const char col_gray1[]  = "#222222";
+static const char col_gray2[]  = "#444444";
+static const char col_gray3[]  = "#BBBBBB";
+static const char col_gray4[]  = "#EEEEEE";
+static const char col_cyan[]   = "#005577";
+static const char col_purple[] = "#9544FF";
 
 /* Color schemes */
-static const char *colors[][4]      = {
+static const char *colors[][4] = {
 /*                  FG         BG          Border      Float     */
 	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2,  col_gray2 },
 	[SchemeSel]  = { col_gray4, col_purple, col_purple, col_cyan  },
@@ -103,52 +105,56 @@ static const char *firefoxcmd[] = { "firefox", NULL };
    .f = Float
 */
 static const Key keys[] = {
-	/* Modifier                     Key             Function          Argument */
-   { MODKEY,                       XK_w,           spawn,            { .v = firefoxcmd }},   /* Run firefox */
-	{ MODKEY,                       XK_d,           spawn,            { .v = dmenucmd }},     /* Run dmenu */
-	{ MODKEY,                       XK_Return,      spawn,            { .v = termcmd }},      /* Spawn new ST instance */
-	{ MODKEY,                       XK_b,           togglebar,        { 0 }},                 /* Toggle bar */
-	{ MODKEY,                       XK_j,           focusstack,       { .i = +1 }},           /* Cycle to next window in the stack */
-	{ MODKEY,                       XK_k,           focusstack,       { .i = -1 }},           /* Cycle to the previous window in the stack */
-	{ MODKEY,                       XK_a,           incnmaster,       { .i = +1 }},           /* Add another master window */
-	{ MODKEY,                       XK_s,           incnmaster,       { .i = -1 }},           /* Subtract/demote most recent master to a slave */
-	{ MODKEY,                       XK_h,           setmfact,         { .f = -0.05f }},       /* Move vertical split left */
-	{ MODKEY,                       XK_l,           setmfact,         { .f = +0.05f }},       /* Move vertical split right */
-	{ MODKEY,                       XK_space,       zoom,             { 0 }},                 /* Swap active window with master */
-	{ MODKEY,                       XK_Tab,         view,             { 0 }},                 /* Cycle between current tag and previously selected tag */
-	{ MODKEY,                       XK_q,           killclient,       { 0 }},                 /* Kill active window */
+	/* Modifier                     Key                Function          Argument */
+   { MODKEY,                       XK_w,              spawn,            { .v = firefoxcmd }},   /* Run firefox */
+	{ MODKEY,                       XK_d,              spawn,            { .v = dmenucmd }},     /* Run dmenu */
+	{ MODKEY,                       XK_Return,         spawn,            { .v = termcmd }},      /* Spawn new ST instance */
+	{ MODKEY,                       XK_b,              togglebar,        { 0 }},                 /* Toggle bar */
+	{ MODKEY,                       XK_j,              focusstack,       { .i = +1 }},           /* Cycle to next window in the stack */
+	{ MODKEY,                       XK_k,              focusstack,       { .i = -1 }},           /* Cycle to the previous window in the stack */
+	{ MODKEY,                       XK_a,              incnmaster,       { .i = +1 }},           /* Add another master window */
+	{ MODKEY,                       XK_s,              incnmaster,       { .i = -1 }},           /* Subtract/demote most recent master to a slave */
+	{ MODKEY,                       XK_h,              setmfact,         { .f = -0.05f }},       /* Move vertical split left */
+	{ MODKEY,                       XK_l,              setmfact,         { .f = +0.05f }},       /* Move vertical split right */
+	{ MODKEY,                       XK_space,          zoom,             { 0 }},                 /* Swap active window with master */
+	{ MODKEY,                       XK_Tab,            view,             { 0 }},                 /* Cycle between current tag and previously selected tag */
+	{ MODKEY,                       XK_q,              killclient,       { 0 }},                 /* Kill active window */
    /* Layouts */
-	{ MODKEY,                       XK_t,           setlayout,        { .v = &layouts[0] }},  /* Tiling */
-	{ MODKEY,                       XK_f,           setlayout,        { .v = &layouts[1] }},  /* Floating */
-	{ MODKEY,                       XK_m,           setlayout,        { .v = &layouts[2] }},  /* Monicle */
-   { MODKEY,                       XK_u,           setlayout,        { .v = &layouts[3] }},  /* Bottom stack (vertical) */
-   { MODKEY,                       XK_i,           setlayout,        { .v = &layouts[4] }},  /* Bottom stack (horizontal) */
-   { MODKEY,                       XK_o,           setlayout,        { .v = &layouts[5] }},  /* Centered master */
-   { MODKEY,                       XK_p,           setlayout,        { .v = &layouts[6] }},  /* Centered floating master */
-   { MODKEY,                       XK_c,           setlayout,        { .v = &layouts[7] }},  /* Columns */
-   { MODKEY,                       XK_g,           setlayout,        { .v = &layouts[8] }},  /* Grid */
+	{ MODKEY,                       XK_t,              setlayout,        { .v = &layouts[0] }},  /* Tiling */
+	{ MODKEY,                       XK_f,              setlayout,        { .v = &layouts[1] }},  /* Floating */
+	{ MODKEY,                       XK_m,              setlayout,        { .v = &layouts[2] }},  /* Monicle */
+   { MODKEY,                       XK_u,              setlayout,        { .v = &layouts[3] }},  /* Bottom stack (vertical) */
+   { MODKEY,                       XK_i,              setlayout,        { .v = &layouts[4] }},  /* Bottom stack (horizontal) */
+   { MODKEY,                       XK_o,              setlayout,        { .v = &layouts[5] }},  /* Centered master */
+   { MODKEY,                       XK_p,              setlayout,        { .v = &layouts[6] }},  /* Centered floating master */
+   { MODKEY,                       XK_c,              setlayout,        { .v = &layouts[7] }},  /* Columns */
+   { MODKEY,                       XK_g,              setlayout,        { .v = &layouts[8] }},  /* Grid */
    /* Layouts end */
-	{ MODKEY|ShiftMask,             XK_f,           togglefloating,   { 0 }},                 /* Snap a floating window back to its position according to the current layout */
-	{ MODKEY,                       XK_0,           view,             { .ui = ~0 }},          /* Display all tags at once */
-	{ MODKEY|ShiftMask,             XK_0,           tag,              { .ui = ~0 }},          /* Duplicate active window handle to all tags */
-	{ MODKEY,                       XK_comma,       focusmon,         { .i = -1 }},           /* Switch active monitor to the left */
-	{ MODKEY,                       XK_period,      focusmon,         { .i = +1 }},           /* Switch active monitor to the right */
-	{ MODKEY|ShiftMask,             XK_comma,       tagmon,           { .i = -1 }},
-	{ MODKEY|ShiftMask,             XK_period,      tagmon,           { .i = +1 }},
-   { MODKEY|ShiftMask,             XK_equal,       setgaps,          { .i = -5 }},           /* Decrease gaps */
-   { MODKEY,                       XK_equal,       setgaps,          { .i = 0 }},            /* Reset gaps */
-   { MODKEY,                       XK_minus,       setgaps,          { .i = +5 }},           /* Increase gaps */
+	{ MODKEY|ShiftMask,             XK_f,              togglefloating,   { 0 }},                 /* Snap a floating window back to its position according to the current layout */
+	{ MODKEY,                       XK_0,              view,             { .ui = ~0 }},          /* Display all tags at once */
+	{ MODKEY|ShiftMask,             XK_0,              tag,              { .ui = ~0 }},          /* Duplicate active window handle to all tags */
+	{ MODKEY,                       XK_comma,          focusmon,         { .i = -1 }},           /* Switch active monitor to the left */
+	{ MODKEY,                       XK_period,         focusmon,         { .i = +1 }},           /* Switch active monitor to the right */
+	{ MODKEY|ShiftMask,             XK_comma,          tagmon,           { .i = -1 }},
+	{ MODKEY|ShiftMask,             XK_period,         tagmon,           { .i = +1 }},
+   { MODKEY|ShiftMask,             XK_equal,          setgaps,          { .i = -5 }},           /* Decrease gaps */
+   { MODKEY,                       XK_equal,          setgaps,          { .i = 0 }},            /* Reset gaps */
+   { MODKEY,                       XK_minus,          setgaps,          { .i = +5 }},           /* Increase gaps */
+   { MODKEY,                       XK_bracketleft,    mpdchange,        { .i = -1 }},           /* Go to previous song */
+   { MODKEY,                       XK_bracketleft,    mpdchange,        { .i = -1 }},           /* Go to previous song */
+   { MODKEY|ShiftMask,             XK_bracketright,   mpdchange,        { .i = +1 }},           /* Go to next song */
+   { MODKEY,                       XK_bracketright,   mpdcontrol,       { 0 }},                 /* Stop song */
    /* Switch between tags */
-	  TAGKEYS(                      XK_1,                             0)
-	  TAGKEYS(                      XK_2,                             1)
-	  TAGKEYS(                      XK_3,                             2)
-	  TAGKEYS(                      XK_4,                             3)
-	  TAGKEYS(                      XK_5,                             4)
-	  TAGKEYS(                      XK_6,                             5)
-	  TAGKEYS(                      XK_7,                             6)
-	  TAGKEYS(                      XK_8,                             7)
-	  TAGKEYS(                      XK_9,                             8)
-	{ MODKEY|ShiftMask,             XK_q,           quit,             { 0 }},                   /* Kill DWM */
+	  TAGKEYS(                      XK_1,                                0)
+	  TAGKEYS(                      XK_2,                                1)
+	  TAGKEYS(                      XK_3,                                2)
+	  TAGKEYS(                      XK_4,                                3)
+	  TAGKEYS(                      XK_5,                                4)
+	  TAGKEYS(                      XK_6,                                5)
+	  TAGKEYS(                      XK_7,                                6)
+	  TAGKEYS(                      XK_8,                                7)
+	  TAGKEYS(                      XK_9,                                8)
+	{ MODKEY|ShiftMask,             XK_q,              quit,             { 0 }},                   /* Kill DWM */
 };
 
 /*** Button events ***/
